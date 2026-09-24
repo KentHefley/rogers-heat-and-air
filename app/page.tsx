9e1c8d6280d8cccf1262f5beaaccfe1a9cc6fd7a
KentHefley <hefleykent@gmail.com>
2026-09-24 13:22:58 -0500

    first commit of the offerings section

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 import Image from "next/image";
 import Header from "../components/header";
+import Offerings from "../components/offerings";
 
 export default function Home() {
   return (
+    <>
     <Header />
+    <Offerings />
+    </>
   );
 }
---
+++ components/offerings.tsx
@@ -0,0 +1,11 @@
+export default function Offerings() {
+    return (
+        <div className="flex flex-col items-center justify-center gap-4 py-10 px-5 md:px-20 lg:px-40 xl:px-60">
+            <div className="flex flex-col items-center">
+                <p className="text-sm font-medium uppercase text-primary">Our Services</p>
+                <h2 className="text-4xl font-bold font-serif">How can we help you?</h2>
+            </div>
+        </div>
+
+    )
+}
